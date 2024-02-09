1235f5f8153ca022453132da9e9e2346b106ed65
Danluis <danluis2a25@gmail.com>
2024-02-09 14:56:53 -0400

    Home Component

---
+++ src/Components/Components_ui/Enlace.jsx
@@ -0,0 +1,9 @@
+export const Enlace = ({children, ...props}) => {
+    return (
+        <li>
+            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            {...props}
+            >{children}</a>
+        </li>
+    )
+}
